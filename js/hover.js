389da2c3cbd77a47b82916c51605e07142af40d9
EB820G2\Bianca <bianca.hoellmueller@gmx.net>
2018-02-28 13:39:49 +0100

    landing page + responsive 28/02/2018

--- js/hover.js
+++ js/hover.js
@@ -3,33 +3,38 @@ $(document).ready(function () {
     //Package-Hover
     $(".package").hover(function () {
         $(this).css({
-            height: '+=20px',
-            width: '+=20px',
+            height: '+=5%',
+            width: '+=5%',
             marginTop: '-=20px',
             fontSize: '+=3px'
             //paddingBottom: '-=20px'
         });
 
         $(this).children("img").css("height", "+=10px");
-        $(this).children(".price").css({paddingTop: '+=20px'});
+        $(this).children(".price").css({paddingTop: '+=10px'});
         $(this).children(".buttons").css({
-            marginLeft: "+=10px",
-            paddingTop: "+=10px"
+            //marginLeft: "+=10px"
+            //paddingTop: "+=10px"
         });
     }, function () {
         $(this).css({
-            height: '-=20px',
-            width: '-=20px',
+            height: '-=5%',
+            width: '-=5%',
             marginTop: '+=20px',
             fontSize: '-=3px'
             //paddingBottom: '+=20px'
         });
 
         $(this).children("img").css("height", "-=10px");
-        $(this).children(".price").css({paddingTop: '-=20px'});
+        $(this).children(".price").css({paddingTop: '-=10px'});
         $(this).children(".buttons").css({
-            marginLeft: "-=10px",
-            paddingTop: "-=10px"
+            //marginLeft: "-=10px"
+            //paddingTop: "-=10px"
         });
+    });
+
+
+    $('#mobilemenu').click(function() {
+        $('#mainmenu').slideToggle('slow');
     });
 });
