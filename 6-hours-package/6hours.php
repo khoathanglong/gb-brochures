7dff332b8b988b5ef18b2ff2167fecc065f71974
unknown <bianca.hoellmueller@gmx.net>
2018-03-13 10:38:03 +0100

    merge long 13/03/2018

--- 6-hours-package/6hours.php
+++ 6-hours-package/6hours.php
@@ -29,8 +29,8 @@
             <div class="logo"><a href="#"><img src="../img/globuzzerLogo.png" alt="gb-logo"></a></div>
             <div id="mobilemenu" class="show-mobile"><span class="fa fa-bars"></span></div>
             <ul id="mainmenu" class="nav-bar">
-                <li><a href="#">Home</a></li>
-                <li><a href="#">My package</a></li>
+                <li><a href="../index.html">Home</a></li>
+                <li><a href="../my-package/my-package.php">My package</a></li>
             </ul>
         </div>
         <div class="img-header package-view">
